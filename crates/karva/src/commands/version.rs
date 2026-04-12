4c39b06776bd0a597c708f8f835e4aac5e6ee6b1
MatthewMckee4 <matthewmckee04@yahoo.co.uk>
2026-04-13 00:51:40 +0100

    Fix incorrect `karva --version`

--- crates/karva/src/commands/version.rs
+++ crates/karva/src/commands/version.rs
@@ -5,11 +5,7 @@ use karva_logging::Printer;
 
 pub fn version() -> Result<()> {
     let mut stdout = Printer::default().stream_for_requested_summary().lock();
-    if let Some(version_info) = crate::version::version() {
-        writeln!(stdout, "karva {}", &version_info)?;
-    } else {
-        writeln!(stdout, "Failed to get karva version")?;
-    }
+    writeln!(stdout, "karva {}", karva_version::version())?;
 
     Ok(())
 }
